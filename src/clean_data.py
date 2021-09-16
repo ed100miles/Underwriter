2e062fe6e8fa2b3131fcef8728adc78fdf8a3b7c
ed100miles <edmilestech@gmail.com>
2021-09-16 22:26:07 +0100

    correlation heatmap and created some new fields

--- src/clean_data.py
+++ src/clean_data.py
@@ -1,5 +1,5 @@
+import matplotlib
 import matplotlib.pyplot as plt
-from numpy.lib.function_base import average
 import pandas as pd
 from pathlib import Path
 import numpy as np
@@ -32,31 +32,57 @@ def get_int64_only_df(df):
     for column in df.columns:
         if df[column].dtype == int:
             int_columns.append(column)
-    int_columns = df[int_columns]
-    return int_columns.copy()
+    ints_df = df[int_columns]
+    return ints_df.copy()
 
 num_only_df = get_int64_only_df(df)
 
-num_only_df.info()
-df.info()
+# num_only_df.info()
+# df.info()
 
-# num_only_df.hist(bins=50)
+# General distributions:
 
-# df.hist(bins=50)
-# plt.show()
+def show_gen_distributions():
+    num_only_df.hist(bins=50)
+    df.hist(bins=50)
+    plt.show()
+# show_gen_distributions()
 
-# column_list = [column for column in df.columns]
-# print(column_list)
-# print(len(column_list))
+'''
+Create some more data?:
+- Income/bluebook = value of car as % of income
+- MVR / age = more points at young age suggests riskier driver
+- Identify collectable cars using bluebook + age?
+- OLDCLAIM / age = many claim in short time of driving suggest risk?
+'''
 
-# print(df.corr())
+# Create new fields:
 
-# plt.matshow(df.corr())
-# # fig, ax = plt.subplots()
+df['BLUE%INCOME*'] = df['INCOME'] / df['BLUEBOOK']  # -0.026 - POOR
+df['PTS_PER_AGE*'] = df['MVR_PTS'] / df['AGE']      # +0.232 - GREAT!
+df['CLM_PER_AGE*'] = df['CLM_FREQ'] / df['AGE']      # +0.234 - GREAT!
+df['RICH_KIDS*'] = df['INCOME'] / df['AGE']         # -0.112 - FAIR
+df['HOME_PER_BLUE*'] = df['HOME_VAL'] / df['BLUEBOOK'] # - 0.04 - POOR
+df['CLM_PER_MILE*'] = df['CLM_FREQ'] / df['TRAVTIME'] # 0.120 - FAIR
+# Check Correlations:
 
-# # ax.set_xticks(df.columns)
-# # ax.set_xticklabels(column for column in df.columns)
+print(df.corr()['CLAIM_FLAG'].abs().sort_values(ascending=False))
 
-# # plt.set_xticks()
-# plt.show()
+num_column_list = [column for column in df.columns if df[column].dtype == int or df[column].dtype == float]
+
+def show_pos_neg_corrs():
+    plt.matshow(df.corr(), cmap=plt.cm.RdYlBu, norm=matplotlib.colors.CenteredNorm())
+    plt.xticks(range(0,len(num_column_list)), labels=num_column_list, rotation='vertical')
+    plt.yticks(range(0,len(num_column_list)), labels=num_column_list, rotation='horizontal')
+    plt.colorbar()
+    plt.show()
+# show_pos_neg_corrs()
+
+def show_abs_corrs():
+    plt.matshow(df.corr().abs(), cmap=plt.cm.Greens)
+    plt.xticks(range(0,len(num_column_list)), labels=num_column_list, rotation='vertical')  
+    plt.yticks(range(0,len(num_column_list)), labels=num_column_list, rotation='horizontal')
+    plt.colorbar()
+    plt.show()
+# show_abs_corrs()
 
